65fda7c86df2a7ab2197d6d932627e63a3c39028
waysketch <waysketch@gmail.com>
2021-03-13 15:03:53 -0700

    Created events to handle client text data.

--- server.js
+++ server.js
@@ -1,5 +1,67 @@
+// ======================== //
+// === Make a WebSocket === //
+// ======================== //
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server: server});
+const wss = new WebSocket.Server({ server: server });
+const PORT = process.env.PORT || 8080
+
+// ================================ //
+// === Connect to the WebSocket === //
+// ================================ //
+const connectToWebSocket = () => {
+    console.log("Starting WebSocket");
+
+    // === Someone Connected === //
+    wss.addListener('connection', function connection(ws){
+
+        // Set isAlive to true on our WebSocket
+        ws.isAlive = true;
+
+        // Log on the Server that someone has connected.
+        console.log("Someone has connected to this socket.");
+
+        // Log a message from a client to the server.
+        ws.on("message", function incoming(message) {
+            console.log("Data: ", message);
+
+            if (message === "off") {
+                console.log("Stop Everything!!!!!");
+                ws.terminate();
+                return;
+            };
+
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                };
+            });
+        });
+    });
+
+    // === Close WebSocket === //
+    wss.on("close", function close() {
+        console.log("Closed this WebSocket.");
+    });
+};
+
+// ======================= //
+// === Make Connection === //
+// ======================= //
+connectToWebSocket();
+
+// ================ //
+// === the page === //
+// ================ //
+app.get("/", ( _ , res) => {
+    res.send("hello world");
+});
+
+// ====================== //
+// === Listen on PORT === //
+// ====================== //
+server.listen(PORT, () => {
+    console.log(`WebSocket is LIVE on PORT: ${PORT}`);
+});
